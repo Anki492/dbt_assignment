3066ea86d834039521ee2517a5abeede0bf7d57f
Ankita Paul <ankita-paul@hcltech.com>
2026-01-19 21:40:39 +0000

    change

--- models/orders.sql
+++ models/orders.sql
@@ -2,26 +2,19 @@
     materialized='incremental',
 
     pre_hook="
-        INSERT INTO DBT_DB.ANALYTICS.MODEL_AUDIT_LOG
+        INSERT INTO DBT_DB.RAW.AUDIT_LOG
         (MODEL_NAME, START_TIME, END_TIME)
         VALUES
         ('incremental_orders', CURRENT_TIMESTAMP, NULL)
     ",
 
     post_hook="
-        UPDATE DBT_DB.ANALYTICS.MODEL_AUDIT_LOG
+        UPDATE DBT_DB.RAW.AUDIT_LOG
         SET END_TIME = CURRENT_TIMESTAMP
         WHERE MODEL_NAME = 'incremental_orders'
           AND END_TIME IS NULL
     "
 ) }}
 
-select
-    order_id,
-    customer_id,
-    order_date,
-    amount,
-    status,
-    updated_at
-from {{ source('raw', 'orders') }}
+
 
