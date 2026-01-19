1ef2175fda1cd1e17a137800af1ea292b61dd450
Ankita Paul <ankita-paul@hcltech.com>
2026-01-19 18:50:50 +0000

    upadate

--- models/incremental_orders.sql
+++ models/incremental_orders.sql
@@ -16,9 +16,3 @@ from {{ source('raw', 'orders') }}
 where updated_at > (select max(updated_at) from {{ this }})
 {% endif %}
 
-{{ config(
-    materialized='incremental',
-    unique_key='order_id',
-    incremental_strategy='merge',
-    merge_update_columns=['amount','status','updated_at']
-) }}
