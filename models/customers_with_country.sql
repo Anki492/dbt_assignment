a526eeda948b7fcf9d06a4beba4e0a275fa1912d
Ankita Paul <ankita-paul@hcltech.com>
2026-01-19 19:30:36 +0000

    update

--- models/customers_with_country.sql
+++ models/customers_with_country.sql
@@ -1,9 +1,2 @@
-SELECT
-    c.customer_id,
-    c.customer_name,
-    c.country_code,
-    s.country_name
-FROM {{ source('raw', 'customers') }} c
-LEFT JOIN {{ ref('country_codes') }} s
-  ON c.country_code = s.country_code;
+SELECT * from {{ ref('country_codes') }};
 
